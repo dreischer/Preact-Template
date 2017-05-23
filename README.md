c3e030d15090a878b5fd3fa4559aa1ecd7f67bc5
dreischer <reischer.david@gmail.com>
2017-05-23 23:27:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+### Install
+`make bootstrap`
+
+### Development
+`make dev`
+Visit `localhost:4000`
+
+### Production
+`make start`
+Visit `localhost:4000`
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  entry: [
+    './app'
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'public/dist/'),
+    publicPath: '/dist/'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: [/node_modules/],
+        loader: 'buble-loader',
+        options: {
+          jsx: 'React.h',
+          objectAssign: 'Object.assign'
+        }
+      },
+      {
+        test: /\.(less|css)$/,
+        loader: 'style-loader!css-loader!less-loader'
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'react': 'preact-compat',
+      'react-dom': 'preact-compat'
+    }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ]
+}
