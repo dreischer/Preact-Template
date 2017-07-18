e74957267690083f2cb166e21fd16276c7846ea6
dreischer <reischer.david@gmail.com>
2017-07-18 12:36:40 +0100

    Remove preact-compat

--- webpack.config.js
+++ webpack.config.js
@@ -28,12 +28,6 @@ module.exports = {
       }
     ]
   },
-  resolve: {
-    alias: {
-      'react': 'preact-compat',
-      'react-dom': 'preact-compat'
-    }
-  },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin()
